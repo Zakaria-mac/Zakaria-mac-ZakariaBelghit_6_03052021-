e65faed488ed85163b13a9e800d2fe5751315142
Zakaria Belghit <zakaria.belghit16@gmail.com>
2021-05-04 15:58:44 +0200

    installation_nodejs_nodemon_express

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const  express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("requête reçue");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.json({ message : "votre requête à bien été reçu"});
+    next()
+});
+
+app.use((req, res) => {
+    console.log ("répose envoyée avec succès")
+})
+
+
+module.exports = app;
